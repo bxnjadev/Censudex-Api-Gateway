aa7ddd6a19e0673bba6eb5d160a5cef3e7548041
Raul Hidalgo <raul.hidalgo@alumnos.ucn.cl>
2025-11-14 01:07:51 -0300

    feat: add authcontroller and postman

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Text;
 using DotNetEnv;
 using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using UserProto;
+using System.Net.Http.Headers;
 
 Env.Load();
 
@@ -16,6 +20,74 @@ builder.Services.AddSwaggerGen(c =>
         Title = "Censudex API Gateway",
         Version = "v1"
     });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
+builder.Services.AddAuthentication(opt => {
+    opt.DefaultAuthenticateScheme =
+    opt.DefaultChallengeScheme =
+    opt.DefaultForbidScheme = 
+    opt.DefaultScheme =
+    opt.DefaultSignInScheme =
+    opt.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
+
+}).AddJwtBearer(options => {
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = Environment.GetEnvironmentVariable("JWT_ISSUER"),
+        ValidateAudience = true,
+        ValidAudience = Environment.GetEnvironmentVariable("JWT_AUDIENCE"),
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("JWT_SIGNING_KEY") ?? throw new ArgumentNullException("Signing key cannot be null or empty."))),
+    };
+    /// <summary>
+    /// token validation event
+    /// </summary>
+    /// <value></value>
+    options.Events = new JwtBearerEvents
+    {
+        OnTokenValidated = async ctx =>
+        {
+            var httpFactory = ctx.HttpContext.RequestServices.GetRequiredService<IHttpClientFactory>();
+            var client = httpFactory.CreateClient("AuthService");
+
+            var token = ctx.Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                ctx.Fail("No token");
+                return;
+            }
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var res = await client.GetAsync("api/login"); 
+            if (!res.IsSuccessStatusCode)
+            {
+                ctx.Fail("Token revoked or invalid");
+            }
+        }
+    };
 });
 builder.Services.AddControllers();
 
@@ -23,6 +95,10 @@ builder.Services.AddControllers();
 builder.Services.AddGrpcClient<UserService.UserServiceClient>(o =>
 {
     o.Address = new Uri(Environment.GetEnvironmentVariable("CLIENTS_SERVICE_URL") ?? "http://localhost:5000");
+});
+builder.Services.AddHttpClient("AuthService", client =>
+{
+    client.BaseAddress = new Uri("http://localhost:5010/"); 
 });
 
 var app = builder.Build();
